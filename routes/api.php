5f0e5a153c861c42523bbc3a1fab1e89e58f7ec8
Латышев Виктор Александрович <v_latyshev@devspark.ru>
2022-08-11 13:30:53 +0300

    Api test
    Специальный тест-роут для отправления запросов на lrs.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
+const JSON_QUERY = '{"actor": {"name": "Иван Иванов","mbox": "mailto:ivanov@mail.ru" }, "verb": { "id": "http://adlnet.gov/expapi/verbs/attempted", "display": { "en-US": "attempted" } }, "object": { "id": "http://mylms.com/activities/course1", "definition": { "name": { "en-US": "Курс 1" } } } }';
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,21 +20,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//todo api/test-show-all-statements <--- не забудь добавить 'api' в url
 
-Route::get('lrsTest', function (){
-    $req = new \Illuminate\Http\Response();
-    $req->header('Authorization', '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG');
-    $response = \Illuminate\Support\Facades\Http::withHeaders([
+Route::get('test-show-10-statement', function (){
+    $response = Http::withHeaders([
         'Authorization' => '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG',
-        'X-Second' => 'bar'
-    ])->post('http://127.0.0.1:8001/api/statements/10', [
-        'name' => 'Taylor',
-    ]);
+    ])->get('http://127.0.0.1:8001/api/statements/10');
     $body = $response->body();
+    var_dump(json_decode($body));
+});
 
-    dd(json_decode($body));
+//todo api/test-show-all-statements <--- не забудь добавить 'api' в url
+Route::get('test-show-all-statements', function (){
+    $response = Http::withHeaders([
+        'Authorization' => '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG',
+    ])->get('http://127.0.0.1:8001/api/statements');
+    $body = $response->body();
+    var_dump(json_decode($body));
+});
 
+//todo api/test-show-all-statements <--- не забудь добавить 'api' в url
 
-    $lrsResponse = \Illuminate\Support\Facades\Http::get('http://127.0.0.1:8001/api/statements');
-    echo $lrsResponse;
+Route::get('test-create-new-statement-in-lrs', function (){
+    $response = Http::withHeaders([
+        'Authorization' => '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG',
+    ])->withBody(JSON_QUERY, 'application/json')
+        ->post('http://127.0.0.1:8001/api/statements');
+    $body = $response->body();
+    echo $body;
+    var_dump($body);
 });
