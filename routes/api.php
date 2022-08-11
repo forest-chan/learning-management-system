c1b55389212de031c1bf2f67ff877e3dcc0e5ea6
Латышев Виктор Александрович <v_latyshev@devspark.ru>
2022-08-11 13:02:11 +0300

    Api test
    Специальный тест-роут для отправления запросов на lrs.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('lrsTest', function (){
+    $req = new \Illuminate\Http\Response();
+    $req->header('Authorization', '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG');
+    $response = \Illuminate\Support\Facades\Http::withHeaders([
+        'Authorization' => '$2y$10$riK8NR.xNdyotQLhkMAWz.NZ78En2a.YTjhFBsPr6jEcUxL.7CDcG',
+        'X-Second' => 'bar'
+    ])->post('http://127.0.0.1:8001/api/statements/10', [
+        'name' => 'Taylor',
+    ]);
+    $body = $response->body();
+
+    dd(json_decode($body));
+
+
+    $lrsResponse = \Illuminate\Support\Facades\Http::get('http://127.0.0.1:8001/api/statements');
+    echo $lrsResponse;
+});
